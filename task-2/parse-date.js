a4e861fbf003804a9b4d43b82e5af8ab6ae95c41
Mustafa Homsi <your-homsi6313@gmail.com>
2026-01-28 09:32:44 +0100

    w3 t2 done

--- task-2/parse-date.js
+++ task-2/parse-date.js
@@ -1,1 +1,25 @@
-// Your code here
+function parseDateString(dateString) {
+    const trimmed = dateString.trim();
+
+    const parts = trimmed.split(/\s+/);
+    const format = parts[0];
+    const datePart = parts[1];
+
+    const [first, second, yearStr] = datePart.split("-");
+
+    const year = Number(yearStr);
+
+    if (format === "MDY") {
+        const month = Number(first);
+        const day = Number(second);
+        return { day, month, year,};
+    }
+
+    const day = Number(first);
+    const month = Number(second);
+    return { day, month, year,};
+}
+console.log(parseDateString("MDY 10-21-1983"));
+console.log(parseDateString("DMY 21-10-1983"));
+console.log(parseDateString("MDY 03-15-2024"));
+console.log(parseDateString("DMY 15-03-2024"));
